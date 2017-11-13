6dbb7bf3e1e6888a9fd8a528b771a43589e39690
Your Name <you@example.com>
2017-11-13 21:44:00 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Telegram Converter Bot .mp3 and .ogg to voice message
---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import telebot
+
+
+TOKEN = '387480715:AAGE4QwIkF6OaTaCln8vB4eAWPwD8Nes_fM'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_command_handelr(message):
+	text = 'Отправьте мне .mp3 или .ogg файл и я сделаю из него голосовое сообщение'
+	bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(content_types=['audio'])
+def audio_handler(message):
+	print(message)
+	file_info = bot.get_file(message.audio.file_id)
+	downloaded_file = bot.download_file(file_info.file_path)
+	bot.send_voice(message.chat.id, downloaded_file)
+
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
