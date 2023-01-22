6dd01db227b2824be46a98308538a5f07e6747a7
Brando Giuffrida <53405682+BrandoDev@users.noreply.github.com>
2023-01-22 17:15:36 +0100

    Update main.py
    Updated script to last version of framework (now 100% works)

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import time
-
 import telebot
-
 
 TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
 
-
 @bot.message_handler(commands=['start'])
-def start_command_handelr(message):
-	text = 'Отправьте мне .mp3 или .ogg файл и я сделаю из него голосовое сообщение'
+def start_command_handler(message):
+	text = "Just send an audio message and i'll resend you as a voice message"
 	bot.send_message(message.chat.id, text)
-
 
 @bot.message_handler(content_types=['audio'])
 def audio_handler(message):
-	print(message)
 	file_info = bot.get_file(message.audio.file_id)
+	#print(f"message.audio.file_id: {message.audio.file_id}\nfile_info: {file_info}\n\n")
 	downloaded_file = bot.download_file(file_info.file_path)
+	#print(f"file_info.file_path: {file_info.file_path}\n")
 	bot.send_voice(message.chat.id, downloaded_file)
 
-
-if __name__ == '__main__':
-	while True:
-		try:
-			bot.polling(none_stop=True)
-		except Exception as e:
-			print(e)
-			time.sleep(30)
+bot.infinity_polling()
